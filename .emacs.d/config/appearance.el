8999d7c63b55b3b62ea01812a6b3aa169f576e03
daniel <daniel@planet-one.local>
2014-07-26 21:35:18 +0200

    fixed and minimized

---
+++ .emacs.d/config/appearance.el
@@ -0,0 +1,27 @@
+(setq visible-bell t
+      font-lock-maximum-decoration t
+      color-theme-is-global t
+      truncate-partial-width-windows nil)
+
+;; Highlight current line
+(global-hl-line-mode 1)
+
+;; Set custom theme path
+(setq custom-theme-directory (concat user-emacs-directory "themes"))
+
+(dolist
+    (path (directory-files custom-theme-directory t "\\w+"))
+  (when (file-directory-p path)
+    (add-to-list 'custom-theme-load-path path)))
+
+;; Default theme
+(load-theme 'default-black)
+
+;; Don't defer screen updates when performing operations
+(setq redisplay-dont-pause t)
+
+;; Highlight matching parentheses when the point is on them.
+(show-paren-mode 1)
+
+
+(provide 'appearance)
