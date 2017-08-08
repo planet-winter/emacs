9b51146769ccff36da01ae664a3a54504e0e99be
planet-winter <dani.winter@gmx.net>
2017-08-08 23:56:45 +0200

    syntax fix

--- test-startup.sh
+++ test-startup.sh
@@ -7,7 +7,7 @@ echo "Attempting startup..."
 ${EMACS:=emacs} -nw --batch \
                 --eval '(let ((debug-on-error t)
                               (url-show-status nil)
-                              (user-init-file (expand-file-name ".emacs.d/init.el"))
+                              (user-init-file (expand-file-name ".emacs.d/init.el")))
                            (load-file user-init-file)
                            (run-hooks (quote after-init-hook)))'
 echo "Startup successful"
